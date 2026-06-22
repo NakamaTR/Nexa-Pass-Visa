240af94752dbd8b40ec543bdd81ccbf0ede8ce91
NakamaTR <namakastoon2001@gmail.com>
2026-06-21 21:03:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('Nexa Pass Visas');
+console.log("NexaPass cargado correctamente 🚀");
+
+
+const cards=document.querySelectorAll(".card");
+
+
+cards.forEach(card=>{
+
+card.addEventListener("mouseenter",()=>{
+
+card.style.background="#d9a44122";
+
+});
+
+
+card.addEventListener("mouseleave",()=>{
+
+card.style.background="#ffffff10";
+
+});
+
+});
